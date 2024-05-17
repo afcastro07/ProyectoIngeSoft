0d831cd543c7ecef49b186641a56c0b62ba2c386
afcastrop07 <acastrop5@ucentral.edu.co>
2024-05-16 19:04:58 -0500

    Cambios en Entidad

--- src/main/java/co/edu/ucentral/Inventario/Servicios/ServiciosProveedor.java
+++ src/main/java/co/edu/ucentral/Inventario/Servicios/ServiciosProveedor.java
@@ -2,6 +2,7 @@ package co.edu.ucentral.Inventario.Servicios;
 
 import co.edu.ucentral.Inventario.Entidadades.Proveedor;
 import co.edu.ucentral.Inventario.Operaciones.OperacionesProveedor;
+import co.edu.ucentral.Inventario.Repositorios.RepositorioProveedor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,9 @@ import java.util.List;
 @Service
 public class ServiciosProveedor implements OperacionesProveedor {
 
+    /**
+     *
+     */
     @Autowired
     RepositorioProveedor repositorioProveedor ;
 
