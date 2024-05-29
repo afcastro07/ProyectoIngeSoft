75a7e17e628a802e1c843a522d23c2ceefa01fbc
afcastro07 <acastrop5@ucentral.edu.co>
2024-05-29 17:08:38 -0500

    resolver conflictos merge

--- src/main/java/co/edu/ucentral/Inventario/InventarioApplication.java
+++ src/main/java/co/edu/ucentral/Inventario/InventarioApplication.java
@@ -1,8 +1,5 @@
 package co.edu.ucentral.Inventario;
 
-import co.edu.ucentral.Inventario.Entidades.Productos;
-import co.edu.ucentral.Inventario.Repositorios.RepositorioProducto;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,13 +12,8 @@ public class InventarioApplication implements CommandLineRunner {
 
 	}
 
-	@Autowired
-	private RepositorioProducto repositorio;
-
 	@Override
 	public void run(String... args) throws Exception {
-		/*Productos producto1 = new Productos("motor", 8000000,8,"electrico","BMW","HZ-HP");
-		repositorio.save(producto1);*/
 
 	}
 }
