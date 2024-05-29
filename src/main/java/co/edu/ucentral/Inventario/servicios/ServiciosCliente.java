a55b347e765d24ae8da09679c169e83ebe073a15
afcastro07 <acastrop5@ucentral.edu.co>
2024-05-29 16:44:23 -0500

    resolver conflictos merge

--- src/main/java/co/edu/ucentral/Inventario/servicios/ServiciosCliente.java
+++ src/main/java/co/edu/ucentral/Inventario/servicios/ServiciosCliente.java
@@ -1,40 +1,44 @@
 package co.edu.ucentral.Inventario.Servicios;
-import co.edu.ucentral.Inventario.Operaciones.OperacionesCliente;
+
 import co.edu.ucentral.Inventario.Entidades.Cliente;
+import co.edu.ucentral.Inventario.Operaciones.OperacionesCliente;
 import co.edu.ucentral.Inventario.Repositorios.RepositorioCliente;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ServiciosCliente implements OperacionesCliente {
+
     @Autowired
     RepositorioCliente repositorioCliente ;
 
     @Override
     public Cliente crear(Cliente cliente) {
-        return RepositorioCliente.save(cliente);
+        return repositorioCliente.save(cliente);
     }
 
     @Override
     public Cliente actualizar(Cliente cliente) {
         if (this.consultarPK(cliente.getId()) != null)
-            return RepositorioCliente.save(cliente);
+            return repositorioCliente.save(cliente);
         return null;
     }
 
     @Override
     public void borrar(Cliente cliente) {
-        RepositorioCliente.delete(cliente);;
+        repositorioCliente.delete(cliente);;
     }
 
     @Override
     public List<Cliente> consultarT() {
-        return RepositorioCliente.findAll();
+        return repositorioCliente.findAll();
     }
 
     @Override
     public Cliente consultarPK(int pk) {
-        Cliente cliente =  RepositorioCliente.findById(pk).orElse(null);
+        Cliente cliente =  repositorioCliente.findById(pk).orElse(null);
         return null;
     }
 }
